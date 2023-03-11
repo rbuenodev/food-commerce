9f51ed19d64ccd4fa1420c24466c1775824957ae
Richard <richard_bueno@ymail.com>
2023-03-11 18:12:15 -0300

    build initianting project

--- front-food-commerce/src/pages/Main/index.tsx
+++ front-food-commerce/src/pages/Main/index.tsx
@@ -1,5 +1,13 @@
+import Sidebar from "../../components/Sidebar";
+import { Container } from "./styles";
+
 const Main: React.FC = () => {
-  return <div>Main</div>;
+  return (
+    <Container>
+      <Sidebar />
+      <h1>Main</h1>
+    </Container>
+  );
 };
 
 export default Main;
